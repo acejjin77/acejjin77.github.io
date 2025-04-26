d1cb69da7c9749ca88eb4d798605682fbdad31ac
acejjin77 <acejjin77@gmail.com>
2025-04-26 10:28:09 +0900

    <update>: 포트폴리오 내용 수정
    1. 요청일자/번호: 2025.04.26
    2. 작업내용
      - 포트폴리오 내용 수정

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,16 +1,46 @@
 /* Change this file to get your personal Portfolio */
 
-// General Information
-export const greeting = {
-  username: "박진성",
-  title: "JAVA 언어 기반 백엔드 개발자",
-  subTitle:
-    "RESTful API, 대용량 데이터 처리, 시스템 안정성 향상 경험 보유, 클린 시큐어 코드와 효율적인 시스템 설계, 트러블 슈팅 다수 경험",
-  resumeLink: "https://yourdomain.com/resume.pdf", // 필요시 연결
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
 };
 
-// Your Skills Section
-export const skillsSection = {
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "박진성",
+  title: "개발하는 백엔드 개발자 박진성입니다.",
+  subTitle: "Java 기반 백엔드 개발자 입니다. Spring boot와 관련된 기술 (JPA, QuesyDSL, REDIS 등) 다수, Infra 관련,  다양한 RDBMS 경험을 보유하고 있습니다.",
+  resumeLink:
+    // "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/acejjin77",
+  gmail: "acejjin7@gmail.com",
+    // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+const skillsSection = {
   skills: [
     "⚡ Java, Spring Boot 3.x 기반 REST API 및 대규모 시스템 구축 경험",
     "⚡ JPA, QueryDSL 활용한 복잡한 데이터 조회 및 최적화",
@@ -33,63 +63,263 @@ export const skillsSection = {
     { skillName: "Git", fontAwesomeClassname: "logos:git-icon" },
     { skillName: "Linux", fontAwesomeClassname: "logos:linux-tux" },
   ],
+  display: true // Set false to hide this section, defaults to true
 };
 
-// Education (선택사항)
-export const degrees = {
-  degrees: [
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
     {
-      title: "한양대학교 에리카캠퍼스",
-      subtitle: "백엔드 개발자",
-      logo_path: "",
-      alt_name: "HYU",
-      duration: "2022.06 - Present",
-      descriptions: [
+      schoolName: "한양대학교 에리카",
+      subHeader: "소프트웨어공학부",
+      duration: "2014.03 - 2020.02",
+      desc: "Participated in the research of XXX and published 3 papers.",
+      descBullets: [
         "자료구조, 네트워크, Linux 시스템 등 기초 전공지식 학습",
         "다양한 언어를 사용한 웹 페이지 다수 제작",
       ]
-    },
-  ],
+    }
+  ]
 };
 
-// Experience
-export const workExperiences = {
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "프론트엔드", //Insert stack or technology you have experience in
+      progressPercentage: "30%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "백엔드",
+      progressPercentage: "100%"
+    },
+    {
+      Stack: "프로그래밍 스킬",
+      progressPercentage: "70%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
   experiences: [
     {
       role: "백엔드 개발자",
       company: "서울의과학연구소",
-      companylogo: "/img/company/seegene.png",
-      date: "2022.06 – Present",
+      companylogo: "./asset/images/scl.png",
+      date: "2022.03 – Present",
       desc: "Spring Boot, Redis, PostgreSQL 기반 시스템 개발 및 유지보수. REST API 설계 및 대규모 데이터 처리 경험 보유.",
     },
   ],
 };
 
-// Projects (선택사항)
-export const projects = {
-  projects: [
-    {
-      name: "건강검진 결과 연동 시스템",
-      description:
-        "외부기관과 FTP 통신 기반으로 결과 데이터를 연동하고, Redis TTL을 활용하여 최신 검사 상태를 실시간 반영.",
-      github_link: "https://github.com/acejjin77", // 필요시 추가
-      link: "https://yourproject.com",
-    },
-    {
-      name: "진단검사 리포트 개선 프로젝트",
-      description:
-        "QueryDSL과 Spring Batch를 활용하여 대량 검사 데이터를 효율적으로 관리 및 배치 처리. 성능 40% 개선 달성.",
-      github_link: "https://github.com/acejjin77",
-    },
-  ],
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
-// Contact Info
-export const contactInfo = {
-  title: "Contact Me",
-  subtitle: "Let's build something amazing together!",
-  number: "+82-10-1234-5678",
-  email_address: "your.email@example.com",
-  github: "https://github.com/acejjin77",
-  linkedin: "https://linkedin.com/in/your-linkedin",
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://saayahealth.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://nextu.se/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      imageAlt: "Google Code-In Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+        },
+        {
+          name: "Award Letter",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        },
+        {
+          name: "Google Code-in Blog",
+          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      imageAlt: "Google Assistant Action Logo",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+92-0000000000",
+  email_address: "saadpasta70@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
 };
